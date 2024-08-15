8f12240b876ffa15753c04a138b8328ab22208a4
akbaroffm <mirolimakbarov171@gmail.com>
2024-08-15 15:10:28 +0500

    change navbar

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -19,90 +19,20 @@ const Navbar = ({ onLanguageChange }) => {
 
   return (
     <div>
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4 relative">
         <div className="flex items-center justify-between p-2">
-          <div>
+          <div className="relative z-20">
             <Link className="inline-block" to={'/'}>
               <img
-                className="logo"
+                className="w-[180px] md:w-[228px]"
                 src={AsakaLogo}
                 alt={t('logoAlt')}
-                width={228}
-                height={28}
               />
             </Link>
           </div>
-          <div className="md:hidden">
-            <button
-              onClick={toggleMenu}
-              className="text-gray-800 focus:outline-none"
-            >
-              {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-            </button>
-          </div>
-          <ul
-            className={`flex-col md:flex-row md:flex items-center space-x-0 md:space-x-[28px] text-[16px] font-medium absolute md:static bg-white w-full md:w-auto transition-all duration-300 ease-in-out ${
-              isOpen
-                ? 'top-16 left-0 opacity-100'
-                : 'top-[-490px] left-0 opacity-0 md:opacity-100'
-            }`}
-          >
-            <NavLink
-              className={({ isActive }) =>
-                `block relative md:inline-block px-4 py-2 md:p-0 animation ${
-                  isActive ? 'text-red-500' : ''
-                }`
-              }
-              to="/vacancies"
-              onClick={() => setIsOpen(false)}
-            >
-              {t('vacancies')}
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `block relative md:inline-block px-4 py-2 md:p-0 animation ${
-                  isActive ? 'text-red-500' : ''
-                }`
-              }
-              to="/digital"
-              onClick={() => setIsOpen(false)}
-            >
-              {t('it-vacancies')}
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `block relative md:inline-block px-4 py-2 md:p-0 animation ${
-                  isActive ? 'text-red-500' : ''
-                }`
-              }
-              to="/career"
-              onClick={() => setIsOpen(false)}
-            >
-              {t('career')}
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `block relative md:inline-block px-4 py-2 md:p-0 animation ${
-                  isActive ? 'text-red-500' : ''
-                }`
-              }
-              to="/blogs"
-              onClick={() => setIsOpen(false)}
-            >
-              {t('blogs')}
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `block relative md:inline-block px-4 py-2 md:p-0 animation ${
-                  isActive ? 'text-red-500' : ''
-                }`
-              }
-              to="/about"
-              onClick={() => setIsOpen(false)}
-            >
-              {t('about')}
-            </NavLink>
-            <div className="flex space-x-2 mt-2 md:mt-0">
+
+          <div className="md:hidden flex items-center space-x-4 absolute right-0 top-1 p-2 z-30">
+            <div className="flex space-x-2">
               <button
                 onClick={() => handleLanguageChange('ru')}
                 className="text-gray-800 focus:outline-none"
@@ -116,7 +46,94 @@ const Navbar = ({ onLanguageChange }) => {
                 UZ
               </button>
             </div>
-          </ul>
+            <button
+              onClick={toggleMenu}
+              className="text-gray-800 focus:outline-none"
+            >
+              {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </button>
+          </div>
+
+          <div className="flex space-x-10">
+            <ul
+              className={`flex-col md:flex-row md:flex items-center space-x-0 md:space-x-[28px] text-[16px] font-medium absolute md:static bg-white w-full md:w-auto transition-all duration-300 ease-in-out z-10 ${
+                isOpen
+                  ? 'top-16 left-0 opacity-100'
+                  : 'top-[-490px] left-0 opacity-0 md:opacity-100'
+              }`}
+            >
+              <NavLink
+                className={({ isActive }) =>
+                  `block relative md:inline-block px-4 py-2 md:p-0 animation ${
+                    isActive ? 'text-red-500' : ''
+                  }`
+                }
+                to="/vacancies"
+                onClick={() => setIsOpen(false)}
+              >
+                {t('vacancies')}
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  `block relative md:inline-block px-4 py-2 md:p-0 animation ${
+                    isActive ? 'text-red-500' : ''
+                  }`
+                }
+                to="/digital"
+                onClick={() => setIsOpen(false)}
+              >
+                {t('it-vacancies')}
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  `block relative md:inline-block px-4 py-2 md:p-0 animation ${
+                    isActive ? 'text-red-500' : ''
+                  }`
+                }
+                to="/career"
+                onClick={() => setIsOpen(false)}
+              >
+                {t('career')}
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  `block relative md:inline-block px-4 py-2 md:p-0 animation ${
+                    isActive ? 'text-red-500' : ''
+                  }`
+                }
+                to="/blogs"
+                onClick={() => setIsOpen(false)}
+              >
+                {t('blogs')}
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  `block relative md:inline-block px-4 py-2 md:p-0 animation ${
+                    isActive ? 'text-red-500' : ''
+                  }`
+                }
+                to="/about"
+                onClick={() => setIsOpen(false)}
+              >
+                {t('about')}
+              </NavLink>
+            </ul>
+
+            <div className="hidden md:flex space-x-2 z-30">
+              <button
+                onClick={() => handleLanguageChange('ru')}
+                className="text-gray-800 focus:outline-none"
+              >
+                RU
+              </button>
+              <button
+                onClick={() => handleLanguageChange('uz')}
+                className="text-gray-800 focus:outline-none"
+              >
+                UZ
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
